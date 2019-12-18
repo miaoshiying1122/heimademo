de6a39c7e03e4b6ef193a328954034607cc3b6a1
miaoshiying <2533498105@qq.com>
2019-12-18 21:01:23 +0800

    第二次

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import '.styles/index.less'
+import './styles/index.less'
 import ElementUI from 'element-ui'
 Vue.config.productionTip = false
 Vue.use(ElementUI)

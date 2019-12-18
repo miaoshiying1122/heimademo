21d045a841640e171d23f593f22aa1cc69f701be
miaoshiying <2533498105@qq.com>
2019-12-18 21:22:33 +0800

    第四次

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-
+import Logins from '../views/login'
 Vue.use(VueRouter)
 
 const routes = [
-  {
+  { // 强制跳转
     path: '/',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
     name: 'home',
     component: Home
+  }, {
+    path: '/login',
+    component: Logins
   }
   // {
   //   path: '/about',

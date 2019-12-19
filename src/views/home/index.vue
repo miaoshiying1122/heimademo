0d2a2034c3616e2efb2d0feb13c7d15a9ecaf7d8
miaoshiying <2533498105@qq.com>
2019-12-19 23:04:28 +0800

    主页页面布局
    git commit -a -m 主页页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,19 @@
 <template>
-    <div>
-    </div>
+<!-- 放置一个外层容器 -->
+<el-container>
+    <!-- 然后左右布局 -->
+    <el-aside>左侧</el-aside>
+    <!-- 放置一个外层容器 -->
+    <el-containe>
+        <!-- 上下布局 -->
+    <el-header>
+    头部
+    </el-header>
+    <el-main>
+    主要区域
+    </el-main>
+    </el-containe>
+</el-container>
 </template>
 
 <script>

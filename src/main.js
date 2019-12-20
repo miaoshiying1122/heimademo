c1c3e29507043505c4c7053690b6d21440b4d25b
miaoshiying <2533498105@qq.com>
2019-12-20 11:50:42 +0800

    注册全局组件及顶部导航栏

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import './styles/index.less'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+import './styles/index.less' // 引入初始化样式
+import ElementUI from 'element-ui' // 引入element包
+import 'element-ui/lib/theme-chalk/index.css' // 引入elementui样式
 import axios from 'axios'
+import Component from './components' // 引入组件对象
 Vue.config.productionTip = false
-Vue.use(ElementUI)
+Vue.use(ElementUI) // 全局注册
+Vue.use(Component) // 注册全局组件对象=>调用该对象中的install方法
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/' // 赋值黑马头条的地址
 Vue.prototype.$axios = axios
 new Vue({
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,24 +1,29 @@
 <template>
-<!-- 放置一个外层容器 -->
+<!-- 放置一个外层容器container -->
 <el-container>
     <!-- 然后左右布局 -->
-    <el-aside>左侧</el-aside>
-    <!-- 放置一个外层容器 -->
-    <el-containe>
+    <el-aside style="background-color:#353b4e; min-height:100vh; width:230px">
+     <layout-aside/>
+    </el-aside>
+    <!-- 在放置一个外层容器container -->
+    <el-container>
         <!-- 上下布局 -->
     <el-header>
-    头部
+     <layout-header/>
     </el-header>
     <el-main>
-    主要区域
+    <!-- 主要区域 -->
     </el-main>
-    </el-containe>
+    </el-container>
 </el-container>
 </template>
 
 <script>
-export default {
 
+export default {
+  components: {
+
+  }
 }
 </script>
 
